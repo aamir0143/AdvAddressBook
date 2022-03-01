840bc619ec05edf26e21efd9cbef0a5c80f0ac7b
aamir0143 <mdaamirreza00@gmail.com>
2022-03-02 00:57:34 +0530

    [Aamir] Add : Create a Address Book Table

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -1,3 +1,14 @@
 ---------------------------------Creating The Database AddressBook(UC1)----------------------------------
 Create Database AddressBookDB;
 Use AddressBookDB;
+---------------------------------Creating The AddressBook Table (UC2)------------------------------------
+Create Table AddressBook(
+	FirstName varchar(50) not null,
+	LastName varchar(50),
+	Address varchar(255),
+	City varchar(50),
+	StateName varchar(50),
+	ZipCode int,
+	PhoneNum bigint,
+	EmailId varchar(50)
+);
