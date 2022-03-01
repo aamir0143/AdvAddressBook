688681ddff69435fc4544e0f8ac4740431b60b5c
aamir0143 <mdaamirreza00@gmail.com>
2022-03-02 02:47:53 +0530

    [Aamir] Add : Draw the ERD For ABS DB

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -54,3 +54,44 @@ Select Count(*) As CountABNames,AddressBookName  From AddressBook Group By Addre
 -----------------------------Adding Same Person In Different AddressBookType(UC11)-----------------------------
 Insert into AddressBook values ('Wasim', 'Anwar', 'Modhipur', 'Purnea', 'Bihar', 854330, 9199523544, 'abc456@gmail.com','FamilyAddressBook','Family');
 Select * From AddressBook Order By FirstName;
+--------------------------------------------Creating Tables Using ER Diagram(UC12)--------------------------------------------------
+Create Table Address_Book(
+	AddressBookId Int Identity(1,1) Primary Key,
+	AddressBookName varchar(50)
+);
+Create Table Persons_Details(
+	PersonId Int Identity(1,1) Primary Key,
+	AddressBookId Int Foreign Key References Address_Book(AddressBookId),
+	FirstName varchar(50),
+	LastName varchar(50),
+	Address varchar(255),
+	City varchar(50),
+	StateName varchar(50),
+	ZipCode int,
+	PhoneNum bigint,
+	EmailId varchar(50)
+);
+CREATE TABLE Person_Types(
+	PersonTypeId Int Identity(1,1) Primary Key,
+	PersonType varchar(50),
+);
+CREATE TABLE Persons_Details_Type(
+	PersonId Int Foreign Key References Persons_Details(PersonId),
+	PersonTypeId Int Foreign Key References Person_Types(PersonTypeId)
+);
+Insert Into Address_Book Values ('Home'),('Office'),('College');
+Select * From Address_Book;
+Insert Into Person_Types Values ('Family'),('Profession'),('Freinds');
+Select * From Person_Types;
+Insert Into Persons_Details Values(1,'Aamir', 'Reza', 'Dubaili', 'Purnea', 'Bihar', 854330, 7975633784, 'aamirreza156@gmail.com'),
+						(3,'Mahesh', 'Babu', 'Manglore', 'Bengluru', 'Karnataka', 560030, 7419632580, 'abc320@gmail.com'),	
+						(3,'Rakesh', 'Kumar', 'Kasba', 'Purnea', 'Bihar', 854330, 9852364170, 'abc852@gmail.com'),	
+						(3,'Ashraf', 'Pasha', 'Gandhinagar', 'Bengluru', 'Karnataka', 560045, 9823434561, 'abc012@gmail.com'),
+						(2,'Aftab', 'Alam', 'Noida', 'Delhi', 'NewDelhi', 965874, 7412589631, 'abc475@gmail.com'),
+						(2,'Afroz', 'Alam', 'Silampur', 'Delhi', 'NewDelhi', 965875, 9874561230, 'abc456@gmail.com'),	
+						(2,'Bhrat', 'Kumar', 'Yalanka', 'Bengluru', 'Karnataka', 560015, 7456981230, 'abc786@gmail.com'),	
+						(1,'Anwar', 'Alam', 'Purnea', 'Purnea', 'Bihar', 854330, 9852741630, 'abc741@gmail.com'),	
+						(1,'Wasim', 'Anwar', 'Modhipur', 'Purnea', 'Bihar', 854330, 9199523544, 'abc456@gmail.com');
+Select * From Persons_Details;
+Insert Into Persons_Details_Type VALUES (1,1),(2,3),(3,3),(4,3),(5,2),(6,2),(7,2),(8,1),(9,1);
+SELECT * FROM Persons_Details_Type;
